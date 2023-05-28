1976bc958ccd2661c077d026be9e53e7f4705525
Sukhbir Singh <sukhbir2506@outlook.com>
2023-05-28 10:24:20 +0530

    School

--- if-else.cpp
+++ if-else.cpp
@@ -1,3 +1,6 @@
+// Problem: if-else condition.
+// Link: https://practice.geeksforgeeks.org/problems/java-if-else-decision-making0924/0
+
 class Solution {
   public:
     string compareNM(int n, int m){
